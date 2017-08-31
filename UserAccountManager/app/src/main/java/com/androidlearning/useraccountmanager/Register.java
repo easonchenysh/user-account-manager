9c1802f5645954643b26b4dea5c3ddb5d74b6b48
ccicnce113424 <ccicnce113424@gmail.com>
2017-08-31 17:09:10 +0800

    正则表达式更正

--- UserAccountManager/app/src/main/java/com/androidlearning/useraccountmanager/Register.java
+++ UserAccountManager/app/src/main/java/com/androidlearning/useraccountmanager/Register.java
@@ -59,10 +59,10 @@ public class Register extends AppCompatActivity {
         String PhoneNumber = RegisterPhoneNumber.getText().toString();
         String EmailAddress = RegisterEmailAddress.getText().toString();
         String PassWord = RegisterPassWord.getText().toString();
-        String regUN = "^.*$";
+        String regUN = "^[a-zA-Z0-9_-]*$";
         String regPN = "^1[3|4|5|7|8][0-9]{9}$";
         String regEA = "^[a-zA-Z_]{1,}[0-9]{0,}@(([a-zA-z0-9]-*){1,}\\.){1,3}[a-zA-z\\-]{1,}$";
-        String regPW = "^.{7-11}$";
+        String regPW = "^[a-zA-Z0-9_-]{7,11}$";
         boolean ok;
         int UserNumber;
         if (UserName.equals("")) {
