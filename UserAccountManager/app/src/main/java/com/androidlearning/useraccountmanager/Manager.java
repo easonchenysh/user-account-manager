971a0beaa10e128c42ca1fcba17673a7b4410fee
ccicnce113424 <ccicnce113424@gmail.com>
2017-09-02 17:39:37 +0800

    更新正则表达式

--- UserAccountManager/app/src/main/java/com/androidlearning/useraccountmanager/Manager.java
+++ UserAccountManager/app/src/main/java/com/androidlearning/useraccountmanager/Manager.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class Manager {
     public static Users users[] = new Users[11];
     public static boolean isInitialization = false;
-    public static String regUN = "^[a-zA-Z0-9_-]*$";
-    public static String regPN = "^1[3|4|5|7|8][0-9]{9}$";
-    public static String regEA = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
-    public static String regPW = "^[a-zA-Z0-9_-]{7,11}$";
+    public static String regUN = "[a-zA-Z0-9_-]*";
+    public static String regPN = "0?(13|14|15|18)[0-9]{9}";
+    public static String regEA = "[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?";
+    public static String regPW = "[a-zA-Z0-9_-]{7,11}";
     public static String getData() {
         SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd   HH:mm:ss");
         return dataFormat.format(new Date());
